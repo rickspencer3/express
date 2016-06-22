b105c73c2bd83e2a70a70eecb77edfa1b3771f0c
Rick Spencer <richard.linger.spencer.3@gmail.com>
2016-06-22 12:19:19 +0200

    first-new-route
    Added the api route, the first route

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./config/mongodb');
-
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var api = require('./routes/api');
 var app = express();
 
 // view engine setup
@@ -27,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/api', routes)
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
