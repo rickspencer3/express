f1aedac9432087c72486f18f750c43af64c0f415
Rick Spencer <richard.linger.spencer.3@gmail.com>
2016-06-22 13:37:03 +0200

    foods-api-start
    Added some routes to figure out how to specify an API

--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a structure regarding the api');
 });
 
+router.get('/foods', function(req, res, next) {
+   res.send({foods:[]})
+});
+router.get('/foods/:search', function(req, res, next) {
+   res.send({search:req.params.search})
+});
+router.get('/foods/id', function(req, res, next) {
+  res.send({id:req.params.id})
+});
 module.exports = router;
