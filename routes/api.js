7ded80bdfea2983c870fb8dec56f4fe2ed1050e3
Rick Spencer <richard.linger.spencer.3@gmail.com>
2016-06-23 15:09:42 +0200

    populate-db
    modified mongoldb.js to insert foods into the database to support
    development

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+console.log('configuring MongoDB: ' + process.env.DATABASE_URL)
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL configured in your docker-compose.yml file
+var url = process.env.DATABASE_URL
+
+
+
 http://192.168.99.100:3000/api/
 router.get('/', function(req, res, next) {
   res.send('respond with a structure regarding the api');
@@ -8,12 +18,18 @@ router.get('/', function(req, res, next) {
 
 http://192.168.99.100:3000/api/foods
 router.get('/foods', function(req, res, next) {
-   if(!req.query.search) {
-     res.send({foods:[]})
-   }
-   else {
-     res.send({results:{term:req.query.search,foods:[]}})
-   }
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+
+     if(!req.query.search) {
+
+       res.send(db.find())
+     }
+     else {
+       res.send({results:{term:req.query.search,foods:[]}})
+     }
+     db.close();
+   });
 });
 
 //http://192.168.99.100:3000/api/foods/1234
