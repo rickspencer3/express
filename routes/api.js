71ff18d47be07e81c143e10abd021821f0339f8f
Rick Spencer <richard.linger.spencer.3@gmail.com>
2016-06-24 10:39:55 +0200

    first-query
    made the findall function work

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-console.log('configuring MongoDB: ' + process.env.DATABASE_URL)
-
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-
-// Connection URL configured in your docker-compose.yml file
-var url = process.env.DATABASE_URL
-
-
-
 http://192.168.99.100:3000/api/
 router.get('/', function(req, res, next) {
   res.send('respond with a structure regarding the api');
@@ -18,27 +8,32 @@ router.get('/', function(req, res, next) {
 
 http://192.168.99.100:3000/api/foods
 router.get('/foods', function(req, res, next) {
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
 
-     if(!req.query.search) {
+  var results = [];
+   if(!req.query.search) {
+     var cursor = db.collection("foods").find();
 
-       res.send(db.find())
-     }
-     else {
-       res.send({results:{term:req.query.search,foods:[]}})
-     }
-     db.close();
-   });
+     cursor.toArray(function(err, documents) {
+       if(documents != null ) {
+         res.send(documents);
+      }
+    });
+   }
+   else {
+     var cursor = db.collection("foods").find();
+
+     cursor.toArray(function(err, documents) {
+       if(documents != null ) {
+         res.send(documents);
+       }
+     });
+
+   }
 });
 
 //http://192.168.99.100:3000/api/foods/1234
 router.get('/foods/:id', function(req, res, next) {
   res.send({food:{id:req.params.id}})
-});
-
-router.get('/foods/:search', function(req, res, next) {
-   res.send({search:req.params.search})
 });
 
 module.exports = router;
